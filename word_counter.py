3c0b70288383ae6d91425d6dd56419b4c91847eb
moonga43 <mediatemoonga@gmail.com>
2026-02-18 21:49:32 +0200

    Delete word_counter.py

--- word_counter.py
+++
@@ -1,6 +0,0 @@
-def word_counter():
-    sentence = input("Enter a sentence: ")
-    words = sentence.split()
-    print("Number of words:", len(words))
-    print("Number of characters:", len(sentence))
-word_counter()
